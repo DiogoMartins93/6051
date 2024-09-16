2e9123e4b7e06f58b0d5742f83d064608f1aee8a
DiogoMartins93 <diogors_martins@hotmail.com>
2024-09-16 16:15:57 +0100

    Add files via upload

---
+++ omeuprimeirocodigo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    char character;
+
+    printf("Introduza o caracter:\t");
+    scanf("%c", &character);
+
+    
+    printf("       *       \n");
+    printf("      ***      \n");
+    printf("     *****     \n");
+    printf("    *******    \n");
+    printf("       *       \n");
+    puts("ola mundo");
+
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
